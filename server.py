7b22c2dd7766dcdb92c09ad1913b8ba097270a99
Kirill Malakhov <malakhovks@nas.gov.ua>
2020-07-15 13:42:22 +0300

    Setup spooler
    Setup spooler

--- server.py
+++ server.py
@@ -15,13 +15,19 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 # load libraries for string proccessing
 import re, string
 
+# from io import StringIO, BytesIO
+
 # load libraries for API proccessing
 from flask import Flask, jsonify, flash, request, Response, redirect, url_for, abort, render_template
 
 # A Flask extension for handling Cross Origin Resource Sharing (CORS), making cross-origin AJAX possible.
 from flask_cors import CORS
 
-ALLOWED_EXTENSIONS = set(['txt', 'xlsx'])
+# for spooler
+import uwsgi
+from tasks import confor_task
+
+ALLOWED_EXTENSIONS = set(['xlsx'])
 
 __author__ = "Kyrylo Malakhov <malakhovks@nas.gov.ua> and Vitalii Velychko <aduisukr@gmail.com>"
 __copyright__ = "Copyright (C) 2020 Kyrylo Malakhov <malakhovks@nas.gov.ua> and Vitalii Velychko <aduisukr@gmail.com>"
@@ -45,8 +51,86 @@ b'_5#y2L"F4Q8z\n\xec]/'
 # app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.secret_key = os.urandom(42)
 
+
+"""
+# ------------------------------------------------------------------------------------------------------
+# Functions
+# ------------------------------------------------------------------------------------------------------
+# """
+# function that check if an extension is valid
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/')
 def index():
+    return 'Hello, CONFOR!'
+
+"""
+# API ---------------------------------------------------------------------------------------------------
+# """
+
+@app.route('/api/confor/file/find', methods=['POST'])
+def queued_find():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return abort(400)
+
+    file = request.files['file']
+
+    # if user does not select file, browser also submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return abort(400)
+
+    if file and allowed_file(file.filename):
+        projectDir = os.getcwd()
+        # xlsx processing
+        if file.filename.rsplit('.', 1)[1].lower() == 'xlsx':
+            xlsxFileName = secure_filename(file.filename)
+            xlsxFile = secure_filename(file.filename)
+            destination = "/".join([tempfile.mkdtemp(),xlsxFile])
+            file.save(destination)
+            file.close()
+            try:
+                with open(destination,'rb') as xlsxFile:
+                    xlsxFileBinary = xlsxFile.read()
+            except IOError as e:
+                logging.error(e, exc_info=True)
+                return abort(500)
+            toTask = confor_task.spool(project_dir = projectDir.encode(), filename = xlsxFileName.encode(), body = xlsxFileBinary)
+            toTask = toTask.decode('utf-8', errors='ignore')
+            toTask = toTask.rpartition('/')[2]
+            return jsonify({'task': { 'status': 'queued', 'parameters': {'mode':'find', 'encoding': 'Windows-1251'}, 'file': xlsxFileName, 'id': resp}}), 202
+    else:
+        return jsonify({'file': { 'filename': 'not allowed'}}), 400
+
+@app.route('/api/confor/file/find/status')
+def check_find_status():
+    taskID = request.args.get('id')
+    if not os.path.exists('/var/tmp/tasks/confor/' + taskID):
+        return jsonify({'task': {'id': taskID, 'status': False}}), 204
+    if os.path.exists('/var/tmp/tasks/confor/' + task_id):
+        return jsonify({'task': {'id': taskID, 'status': True}}), 200
+
+@app.route('/api/confor/file/task/output')
+def get_output_xml():
+    taskID = request.args.get('id')
+    if not os.path.exists('/var/tmp/tasks/confor/' + taskID):
+        return jsonify({'task': taskID, 'status': False}), 204
+    if not os.path.isfile('/var/tmp/tasks/confor/' + taskID + '/output.xml'):
+        return jsonify({'task': task_id, 'status': False}), 204
+
+    try:
+        safe_path = safe_join('/var/tmp/tasks/confor/' + taskID, 'output.xml')
+        return send_file(safe_path, conditional=True, mimetype='text/xml')
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        return abort(500)
+
+@app.route('/api/confor/file/nofind', methods=['POST'])
+def noFind():
     return 'Hello, CONFOR!'
 
 if __name__ == '__main__':
---
+++ tasks.py
@@ -0,0 +1,50 @@
+# load misc utils
+import time, logging, sys, os, tempfile, subprocess
+import traceback
+import uwsgi
+from uwsgidecorators import spool
+import shutil, os, re, string
+
+# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
+
+@spool
+def confor_task(args):
+    try:
+        logging.debug('Start task execution')
+        # data size in bytes
+        logging.debug('Data size in bytes: ' + str(len(args['body'])))
+        projectDir = str(args['project_dir'])
+        pathToConfor = os.path.join(projectDir, 'deploy', 'confor', 'Service3.jar')
+        destinationTempXlsx = "/".join([tempfile.mkdtemp(),'xlsxFile.xlsx'])
+        destinationOutputXml = "/".join([tempfile.mkdtemp(),'output.xml'])
+        try:
+             with open(destinationTempXlsx, 'w') as tempXlsx:
+                tempXlsx.write(args['body'])
+        except IOError as e:
+            logging.error(traceback.format_exc())
+            # logging.error(repr(e))
+            return uwsgi.SPOOL_IGNORE
+        args = ["java", '-jar', pathToConfor, destinationTempXlsx, destinationOutputXml]
+        try:
+            code = subprocess.call(args, stdout=subprocess.DEVNULL)
+            if code == 0:
+                logging.debug("subprocess.call (confor --> output.xml): Success!")
+            else:
+                logging.error("subprocess.call (confor --> output.xml): Error!")
+        except OSError as e:
+            logging.error(e, exc_info=True)
+            return abort(500)
+        # copy output.xml file with results to unique folder with id
+        try:
+            shutil.copy2(destinationOutputXml, '/var/tmp/tasks/confor/' + args['spooler_task_name'])
+        except Exception as e:
+            logging.error(traceback.format_exc())
+            # logging.error(repr(e))
+            return uwsgi.SPOOL_IGNORE
+        return uwsgi.SPOOL_OK
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        # logging.error(repr(e))
+        return uwsgi.SPOOL_IGNORE
