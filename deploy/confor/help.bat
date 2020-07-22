7e5484e9455d171bf31adb9240195c200269ec9f
Vitalii Velychko <50952980+Velychko-Vitalii@users.noreply.github.com>
2020-07-22 22:54:54 +0300

    Update help.bat

--- deploy/confor/help.bat
+++ deploy/confor/help.bat
@@ -1,4 +1,5 @@
 java -jar Service3.jar diabet_demo_learn.xlsx Find$ sample.xml
+java -jar -Xmx3g Service4.jar input_file_name [output_file_name]
 rem parameters: input_file_name [mode:Find$ Default=Nofind$][encoding: Windows-1251 or UTF-8 Default=Windows-1251] [output_file_name]
 rem  diabet_demo_learn.xlsx - имя входного файла; Find$ - параметр режима работы;  sample.xml - имя выходного файла
 rem jre build 1.8.0_221-b11
