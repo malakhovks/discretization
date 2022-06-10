ae5698b6e20cb1034164bf018182e1e4692e5927
Kyrylo Malakhov <malakhovks@nas.gov.ua>
2022-06-10 11:55:12 +0300

    update scratch code and add data
    update scratch code and add data

--- discretization-by-intervals/discretization-by-intervals.py
+++ discretization-by-intervals/discretization-by-intervals.py
@@ -2,29 +2,16 @@ import pandas as pd
 # load libraries for XML proccessing
 import xml.etree.ElementTree as ET
 
-intervals_petal_length = {
-    'labeled':[],
-    'unlabeled':[]
-}
+def is_number_repl_isdigit(s):
+    """ Returns True is string is a number. """
+    return s.replace('.','',1).isdigit()
 
-intervals_petal_width = {
-    'labeled':[],
-    'unlabeled':[]
-}
+intervals_dict = {}
 
-intervals_sepal_length = {
-    'labeled':[],
-    'unlabeled':[]
-}
-
-intervals_sepal_width = {
-    'labeled':[],
-    'unlabeled':[]
-}
-
+# tree = ET.parse('./data/diabetes/intervals.xml')
 tree = ET.parse('./data/iris_out2.xml')
 root = tree.getroot()
-for attribute in root.findall('Attribute'):
+""" for attribute in root.findall('Attribute'):
     name = attribute.find('Name')
     print(name.text)
     if name.text == 'petal length':
@@ -58,95 +45,124 @@ for attribute in root.findall('Attribute'):
             max_value = interval.find('Max').text
             intervals_sepal_width['labeled'].append({number.text:pd.Interval(float(min_value), float(max_value), closed='both')})
             intervals_sepal_width['unlabeled'].append(pd.Interval(float(min_value), float(max_value), closed='both'))
-            print(number.text)
+            print(number.text) """
 
-print('intervals_petal_length:')
-print(intervals_petal_length)
-index_petal_length = pd.IntervalIndex(intervals_petal_length['unlabeled'])
+for attribute in root.findall('Attribute'):
+    name = attribute.find('Name')
+    print(name.text)
+    intervals_dict.update({name.text: {'intervals': [], 'index': {}}})
 
-print('intervals_petal_width:')
-print(intervals_petal_width)
-index_petal_width = pd.IntervalIndex(intervals_petal_width['unlabeled'])
+print('INTERVALS EMPTY: ')
+print(intervals_dict)
 
-print('intervals_sepal_length:')
-print(intervals_sepal_length)
-index_sepal_length = pd.IntervalIndex(intervals_sepal_length['unlabeled'])
+for attribute in root.findall('Attribute'):
+    name = attribute.find('Name')
+    print(name.text)
+    for interval in attribute.findall('Interval'):
+        min_value = interval.find('Min').text
+        max_value = interval.find('Max').text
+        intervals_dict[name.text]['intervals'].append(pd.Interval(float(min_value), float(max_value), closed='both'))
 
-print('intervals_sepal_width:')
-print(intervals_sepal_width)
-index_sepal_width = pd.IntervalIndex(intervals_sepal_width['unlabeled'])
+print('INTERVALS INTERVALS: ')
+print(intervals_dict)
+
+# intervals_dict['sepal length']['index'] = pd.IntervalIndex(intervals_dict['sepal length']['intervals'])
+
+for key, value in intervals_dict.items():
+    value['index'] = pd.IntervalIndex(value['intervals'])
+
+print('INTERVALS INTERVALS INDEX: ')
+print(intervals_dict)
 
 
 
-# print("\nInteger location for requested label...index_petal_length\n", index_petal_length.get_loc(2.0620000000000003))
-# if isinstance(index_petal_length.get_loc(2.0620000000000003), slice):
-#     print(index_petal_length.get_loc(2.0620000000000003).start)
-# print("\nInteger location for requested label...index_petal_length\n", intervals_petal_length['labeled'][index_petal_length.get_loc(1.4)])
+data = pd.read_csv('./data/input_iris_fix_enc.csv', sep=';', dtype = str)
+# data = pd.read_csv('./data/diabetes/diabetes-for-discret-service.csv', sep=';', dtype = str)
 
-data = pd.read_csv('./data/input_iris_fix_enc.csv', sep=';')
+columns_names_list = data.columns.values.tolist()
 
-for item_index,item in enumerate(data['petal length']):
-    # data.set_value(item_index, 'petal length',index_petal_length.get_loc(item))
-    print(item)
-    print(index_petal_length.get_loc(item))
-    if isinstance(index_petal_length.get_loc(item), slice):
-        data.loc[item_index, 'petal length'] = int(index_petal_length.get_loc(item).start) + 1
-        print("slice! : " + str(index_petal_length.get_loc(item).start + 1))
-    else:
-        data.loc[item_index, 'petal length'] = index_petal_length.get_loc(item) + 1
+for col_name in columns_names_list:
+    if col_name not in ['Object', 'Class']:
+        for item_index,item in enumerate(data[col_name]):
+            print(item)
+            print(type(item))
+            if type(item) is not float:
+                if is_number_repl_isdigit(item):
+                    if isinstance(intervals_dict[col_name]['index'].get_loc(float(item)), slice):
+                        data.loc[item_index, col_name] = int(intervals_dict[col_name]['index'].get_loc(float(item)).start) + 1
+                    else:
+                        data.loc[item_index, col_name] = intervals_dict[col_name]['index'].get_loc(float(item)) + 1
+            else:
+                data.loc[item_index, col_name] = ''
 
-for item_index,item in enumerate(data['petal width']):
-    # data.set_value(item_index, 'petal length',index_petal_length.get_loc(item))
-    print(item)
-    print(index_petal_width.get_loc(item))
-    if isinstance(index_petal_width.get_loc(item), slice):
-        data.loc[item_index, 'petal width'] = int(index_petal_width.get_loc(item).start) + 1
-        print("slice! : " + str(index_petal_width.get_loc(item).start + 1))
-    else:
-        data.loc[item_index, 'petal width'] = index_petal_width.get_loc(item) + 1
 
-for item_index,item in enumerate(data['sepal length']):
-    print(item)
-    print(index_sepal_length.get_loc(item))
-    if isinstance(index_sepal_length.get_loc(item), slice):
-        data.loc[item_index, 'sepal length'] = int(index_sepal_length.get_loc(item).start) + 1
-        print("slice! : " + str(index_sepal_length.get_loc(item).start + 1))
-    else:
-        data.loc[item_index, 'sepal length'] = index_sepal_length.get_loc(item) + 1
 
-for item_index,item in enumerate(data['sepal width']):
-    print(item)
-    print(index_sepal_width.get_loc(item))
-    if isinstance(index_sepal_width.get_loc(item), slice):
-        data.loc[item_index, 'sepal width'] = int(index_sepal_width.get_loc(item).start) + 1
-        print("slice! : " + str(index_sepal_width.get_loc(item).start + 1))
-    else:
-        data.loc[item_index, 'sepal length'] = index_sepal_width.get_loc(item) + 1
 
-data['petal length'] = data['petal length'].astype(int)
-data['petal width'] = data['petal width'].astype(int)
-data['sepal length'] = data['sepal length'].astype(int)
-data['sepal width'] = data['sepal width'].astype(int)
+# for item_index,item in enumerate(data['petal length']):
+
+#     if type(item) is not float:
+#         if is_number_repl_isdigit(item):
+#             if isinstance(index_petal_length.get_loc(float(item)), slice):
+#                 data.loc[item_index, 'petal length'] = int(index_petal_length.get_loc(float(item)).start) + 1
+#                 print("slice! : " + str(index_petal_length.get_loc(float(item)).start + 1))
+#             else:
+#                 data.loc[item_index, 'petal length'] = index_petal_length.get_loc(float(item)) + 1
+#     else:
+#         data.loc[item_index, 'petal length'] = ''
+
+# for item_index,item in enumerate(data['petal width']):
+#     # print('item:')
+#     # print(item)
+#     # print(type(item))
+
+#     if type(item) is not float:
+#         if is_number_repl_isdigit(item):
+#             if isinstance(index_petal_width.get_loc(float(item)), slice):
+#                 data.loc[item_index, 'petal width'] = int(index_petal_width.get_loc(float(item)).start) + 1
+#                 print("slice! : " + str(index_petal_width.get_loc(float(item)).start + 1))
+#             else:
+#                 data.loc[item_index, 'petal width'] = index_petal_width.get_loc(float(item)) + 1
+#     else:
+#         data.loc[item_index, 'petal width'] = ''
+
+# for item_index,item in enumerate(data['sepal length']):
+#     # print('item:')
+#     # print(item)
+#     # print(type(item))
+
+#     if type(item) is not float:
+#         if is_number_repl_isdigit(item):
+#             if isinstance(index_sepal_length.get_loc(float(item)), slice):
+#                 data.loc[item_index, 'sepal length'] = int(index_sepal_length.get_loc(float(item)).start) + 1
+#                 print("slice! : " + str(index_sepal_length.get_loc(float(item)).start + 1))
+#             else:
+#                 data.loc[item_index, 'sepal length'] = index_sepal_length.get_loc(float(item)) + 1
+#     else:
+#         data.loc[item_index, 'sepal length'] = ''
+
+# for item_index,item in enumerate(data['sepal width']):
+#     # print('item:')
+#     # print(item)
+#     # print(type(item))
+
+#     if type(item) is not float:
+#         if is_number_repl_isdigit(item):
+#             if isinstance(index_sepal_width.get_loc(float(item)), slice):
+#                 data.loc[item_index, 'sepal width'] = int(index_sepal_width.get_loc(float(item)).start) + 1
+#                 print("slice! : " + str(index_sepal_width.get_loc(float(item)).start + 1))
+#             else:
+#                 data.loc[item_index, 'sepal width'] = index_sepal_width.get_loc(float(item)) + 1
+#     else:
+#         data.loc[item_index, 'sepal width'] = ''
+
+# data['petal length'] = data['petal length'].astype(int)
+# data['petal width'] = data['petal width'].astype(int)
+# data['sepal length'] = data['sepal length'].astype(int)
+# data['sepal width'] = data['sepal width'].astype(int)
+
+# data['petal length'] = data['petal length'].astype(str)
+# data['petal width'] = data['petal width'].astype(str)
+# data['sepal length'] = data['sepal length'].astype(str)
+# data['sepal width'] = data['sepal width'].astype(str)
 
 data.to_csv("./data/test.csv", index=False, sep=';')
-
-
-# print(data.columns)
-# print(data['sepal length'][0])
-
-
-
-
-
-# interval1 = pd.Interval(50, 75)
-# interval2 = pd.Interval(50, 75)
-# index = pd.IntervalIndex([interval2, interval1])
-# Get integer location for requested label
-# print("\nInteger location for requested label...\n",index.get_loc(75))
-# dict_interval = {pd.Interval(50, 75):"1",pd.Interval(50, 75):"2"}
-
-# intervals_sepal_length = [{"1":pd.Interval(4.3, 4.8759999999999994)},{"2":pd.Interval(4.8759999999999994, 5.272)}]
-
-# index_sepal_length = pd.IntervalIndex([intervals_sepal_length[0]["1"], intervals_sepal_length[1]["2"]])
-
-# print("\nInteger location for requested label...\n", index_sepal_length.get_loc(5.1))
